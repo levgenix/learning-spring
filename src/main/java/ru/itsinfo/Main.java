d1e5a701cf315c993580fba5f21566064cca8459
levgenix <mr.lobach@gmail.com>
2020-10-23 09:03:29 +0300

    Initial Spring context

--- src/main/java/ru/itsinfo/Main.java
+++ src/main/java/ru/itsinfo/Main.java
@@ -1,7 +1,14 @@
 package ru.itsinfo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+
+        context.close();
     }
 }
---
+++ src/main/java/ru/itsinfo/TestBean.java
@@ -0,0 +1,13 @@
+package ru.itsinfo;
+
+public class TestBean {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+}
