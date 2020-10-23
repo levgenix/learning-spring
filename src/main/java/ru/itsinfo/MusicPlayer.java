08c509a673fff8e3eed696eade09e4154d571e08
levgenix <mr.lobach@gmail.com>
2020-10-23 10:36:19 +0300

    Playing playlist

--- src/main/java/ru/itsinfo/MusicPlayer.java
+++ src/main/java/ru/itsinfo/MusicPlayer.java
@@ -1,21 +1,24 @@
 package ru.itsinfo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
 
     private int volume;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> music) {
+        this.musicList = music;
     }
 
     public MusicPlayer() {
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusic(List<Music> music) {
+        this.musicList = music;
     }
 
     public String getName() {
@@ -35,6 +38,8 @@ public class MusicPlayer {
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
