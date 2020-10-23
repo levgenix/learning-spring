f6deee6f6ab9a2e7f5f111ae107a46a2b8d0924a
levgenix <mr.lobach@gmail.com>
2020-10-23 09:33:22 +0300

    Dependency Injection added for Music player

--- src/main/java/ru/itsinfo/Main.java
+++ src/main/java/ru/itsinfo/Main.java
@@ -6,9 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-
-        MusicPlayer player = new MusicPlayer(music);
+        MusicPlayer player = context.getBean("playerBean", MusicPlayer.class);
         player.playMusic();
 
         context.close();
